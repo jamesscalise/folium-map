c6998eafdc00f6179c0ccf8a5d6863001a303d47
James <jamescscalise@gmail.com>
2020-09-06 21:55:08 -0500

    added population polygons

--- map1.py
+++ map1.py
@@ -31,7 +31,11 @@ for lt, ln, el, nm in zip(lat, lon, elev, name):
     fg.add_child(folium.CircleMarker(radius=6, fill=True, location=[lt, ln], popup=folium.Popup(iframe), fill_opacity=0.7, color=color_producer(el)))
 
 
+fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding="utf-8-sig").read()),
+style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000 else 'orange' 
+if 10000000 < x['properties']['POP2005'] < 20000000 else 'red'}))
+
+
 
 map.add_child(fg)
-
 map.save("Map3.html")
